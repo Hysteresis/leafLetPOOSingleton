4725f19db0289ff738a22588f35889fc9351beb3
unknown <piballz@gmail.com>
2022-12-22 14:15:11 +0100

    JS leaflet

--- index.php
+++ index.php
@@ -7,20 +7,20 @@
     <title>LeafletPOOSingleton</title> 
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.3/dist/leaflet.css"
-     integrity="sha256-kLaT2GOSpHechhsozzB+flnD+zUyjE2LlfWPgU04xyI="
-     crossorigin=""/>
-      <!-- Make sure you put this AFTER Leaflet's CSS -->
+    integrity="sha256-kLaT2GOSpHechhsozzB+flnD+zUyjE2LlfWPgU04xyI="
+    crossorigin=""/>
+    <!-- Make sure you put this AFTER Leaflet's CSS -->
     <script src="https://unpkg.com/leaflet@1.9.3/dist/leaflet.js"
-     integrity="sha256-WBkoXOwTeyKclOHuWtc+i2uENFpDZ9YPdf5Hf+D7ewM="
-     crossorigin=""></script>
-     <script defer src="script.js"></script>
-     <link rel="stylesheet" href="newstyle.css">
+    integrity="sha256-WBkoXOwTeyKclOHuWtc+i2uENFpDZ9YPdf5Hf+D7ewM="
+    crossorigin=""></script>
+    <script defer src="script.js"></script>
+    <link rel="stylesheet" href="newstyle.css">
 
 
 </head>
 <body>
     <div id="map"></div>
-   
+
     <?php 
         require_once('Address.php');
         require_once('Config.php');
@@ -50,6 +50,10 @@ echo "</table>";
 
 
 ?>
+
+<!-- L'utilisateur saisit latitude et longitude puis inserer un marker dans la map -->
+
+
 <!-- html -->
 <!-- mettre un id a table -->
 <!-- mettre une class a chaque balise td -->
@@ -57,17 +61,6 @@ echo "</table>";
 <!-- JS -->
 <!-- recuperer les enfants de table tr td -->
 <!-- afficher nos var dans marker en js-->
-<script>
-    // let table = document.getElementById('table');
-    // let enfantTableTr = table.childNodes;
-    // let enfantTableTrTd = enfantTableTr.childNodes;
-    // console.log(enfantTableTr[1]);
-
-</script>
-
-
-
-
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
 <script>
     $(document).ready(function(){
@@ -78,30 +71,8 @@ echo "</table>";
             var ville = this.getElementsByClassName("ville")[0].innerHTML;
             var description = this.getElementsByClassName("description")[0].innerHTML;
             var marker = L.marker([Number(lat), Number(long)]).addTo(map);
-            
-     
-
-//             console.log(latitude)
-//             // console.log(longitude)
-// var latitudes = document.getElementsByClassName("latitude");
-//         for (var latitude of latitudes) {
-//         console.log(latitude);
-//         };
-//         var longitudes = document.getElementsByClassName("longitude");
-//         for (var longitude of longitudes) {
-//         console.log(longitude);
-        
-        // };
-       
-        // var villes = document.getElementsByClassName("ville");
-        // for (var ville of villes) {
-        // console.log(ville);
         });
-
-
   });
-
-
 </script>
 </body>
 </html>
